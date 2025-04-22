a2ad883615ddec12877efd221f5d881e8a068708
BATCOMPUTER\Sebastian <Sebastianrr1390@gmail.com>
2025-04-22 15:20:31 -0500

    Controlador de login, y logica para tokens de validacion

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Matriculas.Clases;
+using Matriculas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Matriculas.Controllers
+{
+    [RoutePrefix("api/Login")]
+    [AllowAnonymous]
+    public class LoginController : ApiController
+    {
+        [HttpPost]
+        [Route("Ingresar")]
+        public IQueryable<LoginRespuesta> Ingresar(Login login)
+        {
+            clsLogin _Login = new clsLogin();
+            _Login.login = login;
+            return _Login.Ingresar();
+        }
+    }
+}
